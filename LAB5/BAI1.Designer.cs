8b5b91189ee7ac574aed7aef0c569c7750c67850
NBNgoc-2k1 <bongmasanco934@gmail.com>
2021-06-18 23:02:40 +0700

    commit bai 1

---
+++ LAB5/BAI1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace LAB5
+{
+    partial class BAI1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BAI1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.senderEmail = new System.Windows.Forms.TextBox();
+            this.receiverEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.senderPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bodyMail = new System.Windows.Forms.TextBox();
+            this.subjectMail = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To";
+            // 
+            // senderEmail
+            // 
+            this.senderEmail.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.senderEmail.Location = new System.Drawing.Point(114, 26);
+            this.senderEmail.Multiline = true;
+            this.senderEmail.Name = "senderEmail";
+            this.senderEmail.Size = new System.Drawing.Size(355, 36);
+            this.senderEmail.TabIndex = 2;
+            // 
+            // receiverEmail
+            // 
+            this.receiverEmail.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiverEmail.Location = new System.Drawing.Point(114, 81);
+            this.receiverEmail.Multiline = true;
+            this.receiverEmail.Name = "receiverEmail";
+            this.receiverEmail.Size = new System.Drawing.Size(355, 36);
+            this.receiverEmail.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(554, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // senderPass
+            // 
+            this.senderPass.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.senderPass.Location = new System.Drawing.Point(677, 17);
+            this.senderPass.Multiline = true;
+            this.senderPass.Name = "senderPass";
+            this.senderPass.PasswordChar = '*';
+            this.senderPass.Size = new System.Drawing.Size(299, 36);
+            this.senderPass.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Body";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 27);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Subject";
+            // 
+            // bodyMail
+            // 
+            this.bodyMail.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bodyMail.Location = new System.Drawing.Point(114, 214);
+            this.bodyMail.Multiline = true;
+            this.bodyMail.Name = "bodyMail";
+            this.bodyMail.Size = new System.Drawing.Size(862, 244);
+            this.bodyMail.TabIndex = 9;
+            // 
+            // subjectMail
+            // 
+            this.subjectMail.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subjectMail.Location = new System.Drawing.Point(114, 159);
+            this.subjectMail.Multiline = true;
+            this.subjectMail.Name = "subjectMail";
+            this.subjectMail.Size = new System.Drawing.Size(862, 36);
+            this.subjectMail.TabIndex = 8;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.sendBtn.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.sendBtn.Location = new System.Drawing.Point(448, 485);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(141, 43);
+            this.sendBtn.TabIndex = 10;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = false;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // BAI1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(998, 540);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.bodyMail);
+            this.Controls.Add(this.subjectMail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.senderPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.receiverEmail);
+            this.Controls.Add(this.senderEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BAI1";
+            this.Text = "Send Email app";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox senderEmail;
+        private System.Windows.Forms.TextBox receiverEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox senderPass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox bodyMail;
+        private System.Windows.Forms.TextBox subjectMail;
+        private System.Windows.Forms.Button sendBtn;
+    }
+}
+
---
+++ LAB5/BAI1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace LAB5
+{
+    public partial class BAI1 : Form
+    {
+        public BAI1()
+        {
+            InitializeComponent();
+        }
+
+        bool IsValidEmail(string mailname)
+        {
+            bool isValidEmail = System.Text.RegularExpressions.Regex.IsMatch(mailname, @"^[^@\s]+@[^@\s]+\.[^@\s]") ;
+            if (string.IsNullOrWhiteSpace(mailname) || !isValidEmail)
+                return false;
+            else return true;
+        }
+
+        bool IsValidString(string str1)
+        {
+            return (string.IsNullOrWhiteSpace(str1));
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            bool isValidSenderEmail = IsValidEmail(senderEmail.Text);
+            bool isValidRecverEmail = IsValidEmail(receiverEmail.Text);
+            bool isBodyMailNull = IsValidString(bodyMail.Text);
+            bool isSubjectMailNull = IsValidString(subjectMail.Text);
+            bool isPasswordNull = IsValidString(senderPass.Text);
+            string mailfrom = senderEmail.Text.ToString().Trim();
+            string mailto = receiverEmail.Text.ToString().Trim();
+            string password = senderPass.Text.ToString().Trim();
+
+            if (isValidRecverEmail && isValidSenderEmail && !isBodyMailNull && !isSubjectMailNull && !isPasswordNull)
+            {
+                using (SmtpClient smtpClient = new SmtpClient("127.0.0.1"))
+                {
+                    var basicCredential = new NetworkCredential(mailfrom, password);
+                    using (MailMessage message = new MailMessage())
+                    {
+                        MailAddress fromAddress = new MailAddress(mailfrom); 
+                        smtpClient.UseDefaultCredentials = false; 
+                        smtpClient.Credentials = basicCredential; 
+                        message.From = fromAddress; 
+                        message.Subject = subjectMail.Text.ToString().Trim();
+                        // Set IsBodyHtml to true means you can send HTML email.
+                        message.IsBodyHtml = false;
+                        message.Body = bodyMail.Text.ToString();
+                        message.To.Add(mailto);
+                        try
+                        {
+                            smtpClient.Send(message);
+                            MessageBox.Show("Email sent succeedly!!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Something wrong\n" + ex.Message, "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (isSubjectMailNull)
+                {
+                    MessageBox.Show("Please enter subject for your mail.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                if (isBodyMailNull)
+                {
+                    MessageBox.Show("Please enter body mail for your mail.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                if (!isValidRecverEmail || !isValidSenderEmail)
+                {
+                    MessageBox.Show("Please ensure email you entered existly ", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                if (isPasswordNull)
+                {
+                    MessageBox.Show("Please enter password of sender mail.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+    }
+}
